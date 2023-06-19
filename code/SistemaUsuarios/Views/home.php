62af5d789a79ef9f0631f9418811a78d7a2cac51
caot140894 <38517315+caot140894@users.noreply.github.com>
2023-06-18 23:09:09 -0500

    Add files via upload

---
+++ code/SistemaUsuarios/Views/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Mini Framework - AbelOSh</title>
+</head>
+<body>
+    <p>Mini Framework V1.0 - <a href="http://localhost/SistemaUsuarios/login">SistemaUsuarios</a></p>
+</body>
+</html>
